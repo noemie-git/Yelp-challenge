25e8e9edbc0a4b2dae26d2f4d52bf6c0760a5d52
Noemie Brancourt <noemie_brancourt@hotmail.com>
2021-11-10 15:31:51 +0100

    Working on validation

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -10,7 +10,7 @@ class ReviewsController < ApplicationController
     @review = Review.new(review_params)
     @review.restaurant = @restaurant
     if @review.save
-      redirect_to restaurant_path(@restaurant)
+      redirect_to restaurant_path(@restaurant), notice: 'Review was successfully added.'
     else
       render :new
     end
